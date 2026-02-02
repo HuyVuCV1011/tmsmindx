eb74ebe905f71fe700dd74952a30c3fc3e2e7a7c
BWCbewchan <bwc.bewchan@gmail.com>
2026-02-02 17:45:48 +0700

    sửa api teacher

--- app/user/training/page.tsx
+++ app/user/training/page.tsx
@@ -74,7 +74,7 @@ export default function TrainingPage() {
       const refreshToken = localStorage.getItem('refreshToken');
       if (refreshToken) {
         try {
-          const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY || '';
+          const FIREBASE_API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY || '';
           const refreshRes = await fetch(`https://securetoken.googleapis.com/v1/token?key=${FIREBASE_API_KEY}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
